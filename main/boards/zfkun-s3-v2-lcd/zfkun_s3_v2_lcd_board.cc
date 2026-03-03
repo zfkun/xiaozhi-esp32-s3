8d90aec6c5f594867867df969ed3daf357bd9380
zfkun <zfkun@msn.com>
2026-03-03 17:30:51 +0800

    fix: typo

--- main/boards/zfkun-s3-v2-lcd/zfkun_s3_v2_lcd_board.cc
+++ main/boards/zfkun-s3-v2-lcd/zfkun_s3_v2_lcd_board.cc
@@ -83,7 +83,7 @@ public:
             gpio_config_t io_conf = {};
             io_conf.intr_type = GPIO_INTR_DISABLE;
             io_conf.mode = GPIO_MODE_OUTPUT;
-            io_conf.pin_bit_mask = (1ULL << AUDIO_PA_EN_GPIO);
+            io_conf.pin_bit_mask = (1ULL << en_pin_);
             io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
             io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
             gpio_config(&io_conf);
